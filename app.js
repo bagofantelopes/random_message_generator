3cecd65e62756ee46236aee1a503f8c4112a96c7
bagofantelopes <agathangalos@gmail.com>
2021-09-15 11:53:29 -0400

    Attempting to split the messages into multiple strings

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 // Load messages from text file
 const fs = require("fs");
 
-var messagesArray = fs.readFileSync('messages.txt').toString().split("\n");
-var randomMessage = Math.floor(Math.random() * arr.length);
+var messagesFirstHalf = fs.readFileSync('messages.txt').toString().split("\n");
+var messagesSecondHalf = fs.readFileSync('messages2.txt').toString().split("\n");
+var randomMessage = Math.floor(Math.random() * messagesFirstHalf.length);
+var randomMessage2 = Math.floor(Math.random() * messagesSecondHalf.length);
 
-
+console.log(messagesFirstHalf[0]);
+console.log(randomMessage);
+console.log(randomMessage2);
+console.log(`${messagesFirstHalf[randomMessage]} + ${messagesSecondHalf[randomMessage2]}`);
