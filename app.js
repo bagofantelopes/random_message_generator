d31674d5319973910d71431cc54d9fa9bfa0e1ef
bagofantelopes <agathangalos@gmail.com>
2021-09-15 12:47:57 -0400

    Attempting to utilize html

--- app.js
+++ app.js
@@ -7,7 +7,5 @@ var messagesSecondHalf = fs.readFileSync('messages2.txt').toString().split("\n")
 var randomMessage = Math.floor(Math.random() * messagesFirstHalf.length);
 var randomMessage2 = Math.floor(Math.random() * messagesSecondHalf.length);
 
-console.log(messagesFirstHalf[0]);
-console.log(randomMessage);
-console.log(randomMessage2);
-console.log(`${messagesFirstHalf[randomMessage]} + ${messagesSecondHalf[randomMessage2]}`);
+console.log(`${messagesFirstHalf[randomMessage]}` + `${messagesSecondHalf[randomMessage2]}`);
+console.log(`${messagesSecondHalf[randomMessage2]}`);
